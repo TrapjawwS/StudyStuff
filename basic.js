2af29e27610ad3ce940a63124cd4279d5dc620e1
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2023-10-25 20:30:18 +0000

    hidden

---
+++ basic.js
@@ -0,0 +1,11 @@
+let typedKeys = "";
+
+document.addEventListener("keydown", function(event) {
+  typedKeys += event.key;
+
+  if (typedKeys === "tboi") {
+    window.location.href = "/workspaces/StudyStuff/tboi.html";
+  } else if (typedKeys.length > 6) {
+    typedKeys = "";
+  }
+});
