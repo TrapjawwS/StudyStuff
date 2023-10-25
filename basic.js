6d74d6bae9bbcf9a8a0126e0a047367eaab46cf8
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2023-10-25 21:52:58 +0000

    yes

--- basic.js
+++ basic.js
@@ -4,7 +4,7 @@ document.addEventListener("keydown", function(event) {
   typedKeys += event.key;
 
   if (typedKeys === "tboi") {
-    window.location.href = "tboi.html";
+    window.location.href = "/workspaces/StudyStuff/basic.js";
   } else if (typedKeys.length > 4) {
     typedKeys = "";
   }
