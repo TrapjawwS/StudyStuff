bebb3017f3d1dfb0de8797b202a6cc3ba8b99e6c
TrapjawwS <145371179+TrapjawwS@users.noreply.github.com>
2023-10-26 17:56:01 +0000

    yes

--- basic.js
+++ basic.js
@@ -4,7 +4,7 @@ document.addEventListener("keydown", function(event) {
   typedKeys += event.key;
 
   if (typedKeys === "1576") {
-    window.location.href = "/public/_redirects/tboi/tboi.html";
+    window.location.href = "/public/_redirects/tboi/test.html";
   } else if (typedKeys.length > 4) {
     typedKeys = "";
   }
